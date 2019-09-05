acf2332e82ec89c55eb3ca0f439874453362ba95
David Gallivan <david.gallivan@strongmind.com>
2019-09-05 16:43:09 -0700

    Added missing files
    Signed-off-by: David Gallivan <david.gallivan@strongmind.com>

---
+++ BlazorStateManagement/SessionState/CounterState.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace BlazorStateManagement.SessionState
+{
+    public class CounterState
+    {
+        private int currentCount = 0;
+
+        public event EventHandler StateChanged;
+
+        public int GetCurrentCount()
+        {
+            return currentCount;
+        }
+        
+        public void SetCurrentCount(int paramCount)
+        {
+            currentCount = paramCount;
+            StateHasChanged();
+        }
+        
+        public void ResetCurrentCount()
+        {
+            currentCount = 0;
+            StateHasChanged();
+        }
+
+        private void StateHasChanged()
+        {
+            // This will update any subscribers
+            // that the counter state has changed
+            // so they can update themselves
+            // and show the current counter value
+            StateChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
